95036df906f87cf0870a6cd2303924b295c81df6
danny10ison <pagesngod@gmail.com>
2023-07-06 10:08:59 +0000

    Chores: Add extra line and break init to meet pycodestyle

--- app/models/account.py
+++ app/models/account.py
@@ -2,8 +2,21 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from app.database.db import Base
 
+
 class Account(Base):
-    """Table format for the Account entity."""
+    """Table format for the Account entity.
+
+    Attributes:
+        __tablename__: the users table name\n
+        id: account id column\n
+        account_number: driver name column\n
+        user_id: driver phone number column\n
+        balance: account balance column\n
+        driver_id: driver id column\n
+        user_id: user id column\n
+        user: defines relationship with user\n
+        driver: defines relationship with driver
+    """
 
     __tablename__ = "accounts"
 
@@ -19,7 +32,12 @@ class Account(Base):
     # Define the relationship to the Driver model
     driver = relationship("Driver", back_populates="account", uselist=False)
 
-    def __init__(self, account_number: str, balance=0, user_id=None, driver_id=None):
+    def __init__(self,
+                 account_number: str,
+                 balance=0,
+                 user_id=None,
+                 driver_id=None
+                 ):
         self.account_number = account_number
         self.balance = balance
         self.user_id = user_id
